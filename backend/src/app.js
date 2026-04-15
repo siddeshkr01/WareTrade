f52662037dc265bd80ac938e76e565ac23965620
siddeshkr01 <siddeshsiddesh748@gmail.com>
2026-04-15 15:37:42 +0530

    Initial full stack setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
+const cors = require("cors");
+
+
 
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 
 // test route
